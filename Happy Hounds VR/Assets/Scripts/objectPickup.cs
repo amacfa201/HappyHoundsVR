9963cb0568ff9348c60d8d7c9ec3b6991af68791
AndrewMac26 <macfarlane.andrew98@gmail.com>
2017-11-23 00:07:14 +0000

    First Implementation of music button
    untested

--- Happy Hounds VR/Assets/Scripts/objectPickup.cs
+++ Happy Hounds VR/Assets/Scripts/objectPickup.cs
@@ -19,6 +19,7 @@ public class objectPickup : MonoBehaviour {
     public GameObject foodPellet;
     public GameObject SpawnPoint;
     public GameObject foodBox;
+    public GameObject musicButton;
     //public int numPellets;
     public int maxPellets = 20;
     public int DisNumPellets;
@@ -28,6 +29,7 @@ public class objectPickup : MonoBehaviour {
     List<GameObject> foodList;
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
 
+    public float buttonDist = 0.45f;
    
     public bool holdingBox;
     public AudioSource foodSource;
@@ -55,6 +57,16 @@ public class objectPickup : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+
+        if (Vector3.Distance(transform.position, musicButton.transform.position) < buttonDist)
+        {
+            GetComponent<SphereCollider>().isTrigger = true;
+        }
+        else
+        {
+            GetComponent<SphereCollider>().isTrigger = false;
+        }
+
 
         DisNumPellets = testScript.numPellets;
         if (controller == null)
