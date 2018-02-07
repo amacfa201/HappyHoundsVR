3d571ebbfc13ac44d263f51e066ed9d50608e9c7
AndrewMac26 <macfarlane.andrew98@gmail.com>
2018-02-07 23:43:23 +0000

    Tried to make grid it didnt work

--- Happy Hounds VR/Assets/Scripts/Dogs/CreateGrid.cs
+++ Happy Hounds VR/Assets/Scripts/Dogs/CreateGrid.cs
@@ -10,11 +10,19 @@ public class CreateGrid : MonoBehaviour {
     public LayerMask untraversableMask;
     float nodeDiameter;
     int gridSizeX, gridSizeY;
+    
 
     void OnDrawGizmos()
     {
         Gizmos.DrawCube(transform.position, new Vector3(gridSize.x, 1, gridSize.y));
-
+        if (nodeGrid != null)
+        {
+            foreach (Node n in nodeGrid)
+            {
+                Gizmos.color = (n.traversable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.nodePos, Vector3.one * (nodeDiameter-0.1f));
+            }
+        }
     }
 
     void Start()
@@ -26,5 +34,19 @@ public class CreateGrid : MonoBehaviour {
     }
 
     void CreateTheGrid()
-    { }
+    {
+        nodeGrid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridSize.x / 2  -Vector3.forward * gridSize.y / 2;
+        for (int i = 0; i < gridSizeX; i++)
+        {
+            for (int j = 0; j < gridSizeY; j++)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.forward * (j * nodeDiameter +nodeRadius);
+                bool traversable = !(Physics.CheckSphere(worldPoint, nodeRadius, untraversableMask));
+                nodeGrid[i, j] = new Node(traversable, worldPoint);
+            }
+        }
+    }
+
+    
 }
