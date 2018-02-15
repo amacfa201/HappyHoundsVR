ac31905340b2a7ac7c8ced8ba318c3c4a33cb032
amacfa201 <amacfa201@caledonian.ac.uk>
2018-02-15 11:43:52 +0000

    Proper Grid(2D) - Next step pathfinding

--- Happy Hounds VR/Assets/Scripts/Dogs/CreateGrid.cs
+++ Happy Hounds VR/Assets/Scripts/Dogs/CreateGrid.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CreateGrid : MonoBehaviour {
 
-    Node[,,] nodeGrid;
-    public Vector3 gridSize;
+    Node[,] nodeGrid;
+    public Vector2 gridSize;
     public float nodeRadius;
     public LayerMask untraversableMask;
     float nodeDiameter;
-    int gridSizeX, gridSizeY, gridSizeZ;
+    int gridSizeX, gridSizeY;
     
 
     void OnDrawGizmos()
     {
-        Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, gridSize.y, gridSize.z));
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridSize.x, 1 ,gridSize.y));
         if (nodeGrid != null)
         {
             foreach (Node n in nodeGrid)
@@ -27,11 +27,11 @@ public class CreateGrid : MonoBehaviour {
                     Gizmos.color = Color.red;
                     Gizmos.DrawCube(n.nodePos, Vector3.one * (nodeDiameter - 0.1f));
                 }
-                //else
-                //{
-                    //Gizmos.color = Color.white;
-                   // Gizmos.DrawCube(n.nodePos, Vector3.one * (nodeDiameter - 0.1f));
-                //}
+                else
+                {
+                    Gizmos.color = Color.white;
+                    Gizmos.DrawCube(n.nodePos, Vector3.one * (nodeDiameter - 0.1f));
+                }
             }
         }
     }
@@ -45,29 +45,37 @@ public class CreateGrid : MonoBehaviour {
         nodeDiameter = nodeRadius * 2;
         gridSizeX = Mathf.RoundToInt(gridSize.x / nodeDiameter);
         gridSizeY = Mathf.RoundToInt(gridSize.y / nodeDiameter);
-        gridSizeZ = Mathf.RoundToInt(gridSize.z / nodeDiameter);
-        CreateTheGrid();
+        //gridSizeZ = Mathf.RoundToInt(gridSize.z / nodeDiameter);
+       // CreateTheGrid();
+        //StartCoroutine(OneTimeUpdate());
+        InvokeRepeating("CreateTheGrid", 0.8f, 2f);
     }
 
-    void CreateTheGrid()
+    public void CreateTheGrid()
     {
-        nodeGrid = new Node[gridSizeX, gridSizeY, gridSizeZ];
+        nodeGrid = new Node[gridSizeX, gridSizeY];
         //Vector3 worldBottomLeft = transform.position - Vector3.right * gridSize.x / 2  -Vector3.forward * gridSize.y / 2 -Vector3.up * gridSize.z/2;
-        Vector3 worldBottomLeft = transform.position - Vector3.right * gridSize.x / 2 - Vector3.up * gridSize.y / 2 - Vector3.forward * gridSize.z / 2;
+        Vector3 worldBottomLeft = transform.position - Vector3.right * gridSize.x / 2 - Vector3.forward * gridSize.y / 2; 
         //print("BL = " + worldBottomLeft);
         for (int i = 0; i < gridSizeX; i++)
         {
             for (int j = 0; j < gridSizeY; j++)
             {
-                for (int k = 0; k < gridSizeZ; k++)
-                {
-                    Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.up * (j * nodeDiameter + nodeRadius) + Vector3.forward * (k * nodeDiameter + nodeRadius);
+
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.forward * (j * nodeDiameter + nodeRadius);
                     //Vector3 worldPoint = worldBottomLeft + Vector3.right * (i * nodeDiameter + nodeRadius) + Vector3.up * (j * nodeDiameter + nodeRadius) + Vector3.right * (k * nodeDiameter + nodeRadius);
                     bool traversable = !(Physics.CheckSphere(worldPoint, nodeRadius, untraversableMask));
-                    nodeGrid[i, j, k] = new Node(traversable, worldPoint);
-                }
+                    nodeGrid[i, j] = new Node(traversable, worldPoint);
+               
             }
         }
+    }
+
+
+    IEnumerator OneTimeUpdate() // Called a few seconds after game start to regenerate grid with the dome doors shut
+    {
+        yield return new WaitForSeconds(0.8f);
+        CreateTheGrid();
     }
 
     
--- Happy Hounds VR/Assets/Scripts/Misc Interactions/OuterDoorScript.cs
+++ Happy Hounds VR/Assets/Scripts/Misc Interactions/OuterDoorScript.cs
@@ -7,13 +7,16 @@ public class OuterDoorScript : MonoBehaviour
     public GameObject doors;
     public GameObject doors2;
     Animator doorAnim;
-
+    public CreateGrid Grid;
+    public bool domeDoor;
     public AudioManager audioManager;
 
     void Start()
     {
         audioManager = FindObjectOfType<AudioManager>();
         doorAnim = GameObject.FindGameObjectWithTag("OuterDoorScript").GetComponent<Animator>();
+        Grid = GameObject.FindGameObjectWithTag("GridGenerator").GetComponent<CreateGrid>();
+        //Grid.CreateTheGrid();
     }
 
     void Update()
@@ -25,16 +28,28 @@ public class OuterDoorScript : MonoBehaviour
     {
         if (other.gameObject.tag == "GameController")
         {
-            if (doorAnim.GetBool("OuterDoorOpen") == true)
+            if (domeDoor)
             {
                 doorAnim.SetBool("OuterDoorOpen", false);
                 audioManager.PlayOnce("DoorOpening");
+                print("DC door to false");
+                domeDoor = false;
+                StartCoroutine(OneTimeUpdate());
             }
             else
             {
                 doorAnim.SetBool("OuterDoorOpen", true);
                 audioManager.PlayOnce("DoorOpening");
+                print("DC door to false");
+                domeDoor = true;
+                StartCoroutine(OneTimeUpdate());
             }
         }
+    }
+
+     IEnumerator OneTimeUpdate() // Called a few seconds after game start to regenerate grid with the dome doors shut
+    {
+        yield return new WaitForSeconds(0.8f);
+        Grid.CreateTheGrid();
     }
 }
